a7b9b5fa665dc0b12b4f30f257a6d717c98df360
hye0nwook <bluehen0315@naver.com>
2024-03-10 04:51:47 +0900

    파이썬 출력

---
+++ test.py
@@ -0,0 +1,45 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print('Hello World!')
+Hello World!
+print('Hello', 'Wprld')
+Hello Wprld
+>>> print('Hello'+'World')
+HelloWorld
+>>> print(!00)
+SyntaxError: invalid syntax
+>>> print(100)
+100
+>>> print(2+4)
+6
+>>> 100
+100
+>>> 10+20
+30
+>>> 10-20
+-10
+>>> 10/20
+0.5
+>>> 123*8765
+1078095
+>>> 10=20*30
+SyntaxError: cannot assign to literal here. Maybe you meant '==' instead of '='?
+>>> 10+20*30
+610
+>>> x=100
+>>> y=200
+>>> x+y
+300
+>>> x-y
+-100
+>>> x*y
+20000
+>>> x/y
+0.5
+>>> x*2
+200
+>>> x**2
+10000
+>>> n=5
+>>> n**2
+25
