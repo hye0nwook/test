a4550e350c660578a863ee90ff79c28fc955b653
hye0nwook <bluehen0315@naver.com>
2024-03-14 01:00:21 +0900

    chap2

---
+++ OneDrive/바탕 화면/chap2/ex13,15 chap2.py
@@ -0,0 +1,18 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> PI=3.141592
+>>> int(input('원의 반지름을 입력하세요 : '))
+원의 반지름을 입력하세요 : 11
+11
+>>> print('원의 둘레 =', 2*11*PI)
+원의 둘레 = 69.115024
+>>> print('원의 면적 =', 11*11*PI)
+원의 면적 = 380.132632
+>>> 
+>>> 
+>>> 
+>>> a=3
+>>> b=4
+>>> c=(a**2+b**2)**(1/2)
+>>> print('c = ', c)
+c =  5.0
