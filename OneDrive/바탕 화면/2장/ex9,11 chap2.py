b2d81b8185027cc58443dd42a9bf1240cc578599
hye0nwook <bluehen0315@naver.com>
2024-03-21 12:34:39 +0900

    2장

---
+++ OneDrive/바탕 화면/2장/ex9,11 chap2.py
@@ -0,0 +1,42 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+celsius=0
+>>> print('섭씨', 0)
+섭씨 0
+>>> print('화씨', (9/5)*celsius+32)
+화씨 32.0
+>>> celsius=10
+>>> print('섭씨', 10)
+섭씨 10
+>>> print('화씨', (9/5)*celsius+32)
+화씨 50.0
+>>> celsius=20
+>>> print('섭씨', 20)
+섭씨 20
+>>> print('화씨', (9/5)*celsius+32)
+화씨 68.0
+>>> celsius=30
+>>> print('섭씨', 30)
+섭씨 30
+>>> print('화씨', (9/5)*celsius+32)
+화씨 86.0
+>>> celsius=40
+>>> print('섭씨', 40)
+섭씨 40
+>>> print('화씨', (9/5)*celsius+32)
+화씨 104.0
+>>> celsius=50
+>>> print('섭씨', 50)
+섭씨 50
+>>> print('화씨', (9/5)*celsius+32)
+화씨 122.0
+>>> 
+>>> 
+>>> 
+>>> fahrenheit=86
+>>> celsius=(fahrenheit-32)*(5/9)
+>>> int(input('화씨 온도를 입력하세요 : '))
+화씨 온도를 입력하세요 : 86
+86
+>>> print('화씨', fahrenheit, '도는 섭씨', celsius, '도 입니다.')
+화씨 86 도는 섭씨 30.0 도 입니다.
